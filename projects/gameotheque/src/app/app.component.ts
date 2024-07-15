b68a4419e1d13c954057d3fadfb2465417f8eee5
Morgan <lef.morgan@gmail.com>
2024-07-15 11:36:26 +0200

    install material + prepare main menu

--- projects/gameotheque/src/app/app.component.ts
+++ projects/gameotheque/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListGamesComponent } from './features/games/components/list-games/list-games.component';
 import { LearningWhatAboutRxjsComponent } from './features/learning-what-about-rxjs/learning-what-about-rxjs.component';
+import { MainMenuComponent } from './shared/ui/menus/main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListGamesComponent, LearningWhatAboutRxjsComponent],
+  imports: [RouterOutlet, MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projects/gameotheque/src/app/app.routes.ts
+++ projects/gameotheque/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ListGamesComponent } from './features/games/components/list-games/list-games.component';
+import { MainComponent } from './pages/main/main.component';
+import { gamesRoutes } from './features/games/games.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'main',
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+  ...gamesRoutes
+];
