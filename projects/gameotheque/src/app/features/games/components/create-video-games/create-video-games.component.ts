007e079fcba921dd67fcfb32624f5d96b462b009
lefmorgan <lef.morgan@gmail.com>
2024-08-20 16:53:34 +0200

    save without real api and display error

--- projects/gameotheque/src/app/features/games/components/create-video-games/create-video-games.component.ts
+++ projects/gameotheque/src/app/features/games/components/create-video-games/create-video-games.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal} from '@angular/core';
 import { FormVideoGameComponent } from '../form-video-game/form-video-game.component';
 import { VideoGame } from '../../models/video-game';
-
+import { SaveOneVideoGameService } from '../../services/save-one-video-game.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  MatSnackBar,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-video-games',
   standalone: true,
-  imports: [FormVideoGameComponent],
+  imports: [FormVideoGameComponent, MatProgressSpinnerModule],
   templateUrl: './create-video-games.component.html',
   styleUrl: './create-video-games.component.scss'
 })
 export class CreateVideoGamesComponent {
+  private readonly service = inject(SaveOneVideoGameService);
+  private readonly snackbar = inject(MatSnackBar);
+  isSaving = signal(false);
+
+  openSnack(message: string) {
+    console.log('bob', message);
+    this.snackbar.open(message, '', {duration: 3000})
+  }
   saveOne(item: VideoGame): void {
-    console.info('Je save', item)
+    this.isSaving.set(true);
+    this.service.save(item).subscribe({
+      next: savedVideoGame => {
+        console.info(`Video Game saved: ${savedVideoGame.nom}`);
+        this.isSaving.set(false)
+      },
+      error: error => {
+        this.openSnack('Error saving video game')
+        this.isSaving.set(false)
+      }
+    })
   }
 }
